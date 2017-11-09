bd291d68f2237135ddaf11d55c48fb7efb3051ae
Xuqing888 <33504710+Xuqing888@users.noreply.github.com>
2017-11-08 18:42:34 -0800

    Create javascript.js

---
+++ javascript.js
@@ -0,0 +1,8 @@
+document.getElementById("tda").innerHTML=stock_ticker_symbol;
+document.getElementById("tdb").innerHTML=last_price;
+//   document.getElementById("tdc").innerHTML=stock_ticker_symbol;
+//   document.getElementById("tdd").innerHTML=stock_ticker_symbol;
+document.getElementById("tde").innerHTML=open;
+//   document.getElementById("tdf").innerHTML=stock_ticker_symbol;
+document.getElementById("tdg").innerHTML=day_range;
+document.getElementById("tdh").innerHTML=volume;
